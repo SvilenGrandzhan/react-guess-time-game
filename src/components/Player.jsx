0fab7454bb73141d3b71bcaf66400da80a12e885
Svilen Grandzhan <peructep@gmail.com>
2023-12-09 14:03:02 -0700

    Practice useRef one more time 20231209

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,14 @@
 import { useState, useRef } from "react";
 
 export default function Player() {
+  // 1. declare useRef hook
   const inputPlayerName = useRef();
 
   const [playerName, setPlayerName] = useState("");
 
   const handleClick = () => {
+    // 3. Use value of connected JSX element adding at the end *.current.value.
+    // Note: typeof declared useRef variable is an Object
     setPlayerName(inputPlayerName.current.value);
     inputPlayerName.current.value = "";
   };
@@ -14,6 +17,7 @@ export default function Player() {
       <h2>Welcome {playerName ?? ""}</h2>
       <p>
         <input
+          // 2. connect useRef hook to special "ref" property of JSX element we want to connect
           ref={inputPlayerName}
           type="text"
         />
@@ -22,3 +26,5 @@ export default function Player() {
     </section>
   );
 }
+
+//!Note: useRef hook does NOT trigger re-load of the component
